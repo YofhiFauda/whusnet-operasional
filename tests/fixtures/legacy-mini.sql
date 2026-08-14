6b2aa342ceede5864db3e0e39a08f3ee85d8dfe8
YofhiFauda <yofhi132@gmail.com>
2026-08-14 15:28:07 +0700

    remove Null

---
+++ tests/fixtures/legacy-mini.sql
@@ -0,0 +1,88 @@
+-- Fixture mini hasil ekstraksi dari dump legacy asli. Jangan diedit manual.
+
+INSERT INTO `cabang` (`ID`, `CABANG`, `LAT_PERUSAHAAN`, `LONG_PERUSAHAAN`) VALUES
+('1', 'JETIS', '-8', '110');
+
+INSERT INTO `paket` (`KODEPAKET`, `NAMA_PAKET`, `JENIS_PAKET`, `KATEGORI_PAKET`, `HARGA`, `SPEEDUP`, `SPEEDDOWN`, `LIMITUP`, `LIMITDOWN`, `PROFILOLT`, `PROFILPPP`, `BONUS`, `KETERANGAN`) VALUES
+('PK000002', 'Up to 2 Mbps 110k', 'Praba', '01.2', '110000', '2000', '2000', '500', '500', '', '', '', ''),
+('PK000003', 'Up to 5 Mbps 165k', 'Praba', '01.2', '165000', '5000', '5000', '1250', '1250', '', '', '', ''),
+('PK000004', 'Up to 10 Mbps 198k', 'Praba', '01.2', '198000', '10000', '10000', '2500', '2500', '', '', '', ''),
+('PK000005', 'Up to 20 Mbps 235k', 'Praba', '01.2', '235000', '20000', '20000', '5000', '5000', '', '', '', ''),
+('PK000006', 'Up to 50 Mbps 530k', 'Praba', '01.2', '530000', '50000', '50000', '12500', '12500', '', '', '', ''),
+('PK000007', 'Up to 100 Mbps 820k', 'Praba', '01.2', '820000', '100000', '100000', '25000', '25000', '', '', '', ''),
+('PK000008', 'Up to 3 Mbps 150k', 'Praba', '01.2', '150000', '3000', '3000', '1000', '1000', '', '', '', ''),
+('PK000009', 'Barbar UpTo 7M', 'Praba', '01.2', '130000', '7000', '7000', '1250', '1250', '', '', '', ''),
+('PK000010', 'Barbar UpTo 13M', 'Praba', '01.2', '165000', '13000', '13000', '2500', '2500', '', '', '', ''),
+('PK000011', 'Barbar UpTo 20M', 'Praba', '01.2', '198000', '20000', '20000', '5000', '5000', '', '', '', ''),
+('PK000012', 'Barbar UpTo 30M', 'Praba', '01.2', '290000', '30000', '30000', '7500', '7500', '', '', '', ''),
+('PK000013', 'Barbar UpTo 50M', 'Praba', '01.2', '330000', '50000', '50000', '12500', '12500', '', '', '', ''),
+('PK000014', 'Barbar UpTo 150M', 'Praba', '01.2', '560000', '150000', '150000', '37500', '37500', '', '', '', ''),
+('PK000015', 'Barbar UpTo 10M', '01.1', '01.2', '150000', '10000', '10000', '2500', '2500', '', '', '', ''),
+('PK000016', 'Barbar UpTo 5M', '01.1', '01.2', '110000', '5000', '5000', '1250', '1250', '', '', '', ''),
+('PK000018', 'Paket UPTO 200mbps', 'Praba', '01.2', '770000', '200000', '200000', '100000', '100000', '', '', '', ''),
+('PK000019', 'Broadband Sekolah-20', 'Praba', '01.2', '220000', '20000', '20000', '5000', '5000', '', '', '', ''),
+('PK000020', 'Whus Speed Up To 5 M', 'Praba', '01.2', '115000', '5000', '5000', '0', '0', '', '', '', ''),
+('PK000021', 'Whus Speed Up To 13', 'Praba', '01.2', '138000', '13000', '13000', '0', '0', '', '', '', ''),
+('PK000022', 'Whus Speed Up To 15', 'Praba', '01.2', '150000', '15000', '15000', '0', '0', '', '', '', ''),
+('PK000023', 'Whus Speed Up To 25', 'Praba', '01.2', '167000', '25000', '25000', '0', '0', '', '', '', ''),
+('PK000024', 'Whus Speed Up To 30', 'Praba', '01.2', '175000', '30000', '30000', '0', '0', '', '', '', ''),
+('PK000025', '111K up to 5M', 'Praba', '01.2', '111000', '5000', '5000', '5000', '5000', '', '', '', ''),
+('PK000026', '111K up to 10M', 'Praba', '01.2', '111000', '10000', '10000', '10000', '10000', '', '', '', ''),
+('PK000027', '165K up to 25M', 'Praba', '01.2', '165000', '25000', '25000', '25000', '25000', '', '', '', ''),
+('PK000028', '198K up to 50M', 'Praba', '01.2', '198000', '50000', '50000', '50000', '50000', '', '', '', ''),
+('PK000029', '150K up to 15M', 'Praba', '01.2', '150000', '15000', '15000', '15000', '15000', '', '', '', ''),
+('PK000030', '110K up to 10M', 'Praba', '01.2', '110000', '10000', '10000', '10000', '10000', '', '', '', ''),
+('PK000031', '138K up to 13M', '01.1', '01.2', '138000', '13000', '13000', '13000', '13000', '', '', '', ''),
+('PK000032', 'Corporate 100 Mbps', 'Praba', '01.2', '500000', '100', '100', '100', '100', '', '', '', 'Paket Corporate'),
+('PK000033', 'Corporate 200 Mbps', 'Praba', '01.2', '850000', '200', '200', '200', '200', '', '', '', 'Paket Corporate'),
+('PK000034', 'Corporate 300 Mbps', 'Praba', '01.2', '1100000', '300', '300', '300', '300', '', '', '', 'Paket Corporate'),
+('PK000035', 'NEW 35 Mbps (138K)', 'Praba', '01.2', '138000', '35000', '35000', '0', '0', '', '', '', 'PAKET MERDEKA 2025'),
+('PK000036', 'NEW 45 Mbps (150K)', 'Praba', '01.2', '150000', '45000', '45000', '0', '0', '', '', '', 'PAKET MERDEKA 2025'),
+('PK000037', 'NEW 70 Mbps (165K)', 'Praba', '01.2', '165000', '70000', '69995', '0', '0', '', '', '', 'PAKET MERDEKA 2025'),
+('PK000038', 'NEW 100 Mbps (198K)', 'Praba', '01.2', '198000', '100000', '99993', '0', '0', '', '', '', 'PAKET MERDEKA 2025'),
+('PK000039', 'NEW 115 Mbps (330K)', 'Praba', '01.2', '330000', '115000', '115000', '0', '0', '', '', '', 'PAKET MERDEKA 2025'),
+('PK000040', 'SPESIAL 50Mbps(138K)', '01.1', '01.2', '138000', '50000', '50000', '50000', '50000', '', '', '', ''),
+('PK21000001', 'default', '01.1', '01.2', '0', '1', '1', '1', '1', '10G-contoh', 'PAKET-0K', '-', 'ket'),
+('undefined', 'undefined', '01.1', '01.2', '0', '0', '0', '0', '0', '-', '-', '-', '-');
+
+INSERT INTO `pengguna` (`IDPENGGUNA`, `IDJABATAN`, `IDCABANG`, `NAMADEPAN`, `NAMABELAKANG`, `REKOMENDASI`, `HP`, `TLP`, `IDWILAYAH`, `KOTA`, `KEC`, `DESA`, `ALMT`, `EMAIL`, `STATUSAKUN`, `NAMAPERUSAHAAN`, `NPWP`, `KTP_SIM`, `FOTOKTP`, `JENISKELAMIN`, `JENISPELANGGAN`, `FOTO`, `inserted_at`) VALUES
+('PE000002', '8', '1', 'ardiyanto-cahyo-nugroho', '', '', '628563652226', NULL, '3502100009', 'KABUPATEN PONOROGO', 'JETIS', 'JETIS', 'Dukuh Jetis 1 RT 01 RW 01 Jetis', '0', 'aktif', NULL, '0', '3502092901980001', '4ad6087ddc8417e60589277781c3c1b3.jpg', 'L', 'RUMAHAN', '', '2022-05-10 03:41:27'),
+('PE000003', '8', '1', 'hanif-saifulloh', '', '', '6285855003646', NULL, '3502100014', 'KABUPATEN PONOROGO', 'JETIS', 'WINONG', 'Dukuh Winong 1 RT 01 RW 01 Winong', '0', 'aktif', NULL, '0', '3502090803710001', 'a12c599e0299179662c77eab530183c8.jpeg', 'L', 'RUMAHAN', '', '2022-01-06 08:41:59'),
+('PE000004', '8', '1', 'siti-juariyah', '', '', '6285230330575', NULL, '3502100011', 'KABUPATEN PONOROGO', 'JETIS', 'WONOKETRO', 'Dukuh Jintap RT 01 RW 01 Wonoketro', '0', 'aktif', NULL, '0', '3502095010650003', 'bacbacd2b9580953ff2accf42b7a1fb6.jpeg', 'P', 'RUMAHAN', '', '2022-01-06 08:44:17');
+
+INSERT INTO `prosedure_permintaan_wifi` (`KODEAPP`, `kategori_perangkat_jaringan`, `kode_kontrol_distribusi`, `IDPERMINTAAN`, `IDPENGGUNA`, `STATUS`, `TGL_AKTIFPUTUS`, `STATUSPASANG`, `ALASAN`, `STATUSTINDAKANALAT`, `STATUSLANGGANAN`, `UBAHKONEKSI`, `IDPAKET`, `DISURVEY`, `DIACC`, `DIPROSES`, `CREATED`, `DILAPORKAN`, `TGLDIACC`, `TGLSURVEY`, `TGLDIPROSES`, `TGLSELESAI`, `STATUSALAT`, `VERIFIED`, `VERIFIED_AT`, `JENISJARINGAN`, `JENISMEMBER`, `IDBIAYA`, `inserted_at`, `updated_at`) VALUES
+('C', '1', 'X4A', 'RQ000004', 'PE000002', 'ACTIVE', '0000-00-00', 'Berhasil', '', '', '', '1', 'PK000003', '6', '6', '6', 'PE21000001', '6', '2022-02-07 12:13:42', '2022-02-07 11:43:31', '2022-02-07 12:13:50', '2022-02-08 20:36:19', 'SEWA', '6', '2022-02-08 20:46:45', 'KABEL', '2', 'IN000035', '2022-01-06 08:34:22', '2022-05-10 03:45:55'),
+('C', '1', 'X4C', 'RQ000005', 'PE000003', 'ACTIVE', '0000-00-00', 'Berhasil', '', '', '', '1', 'PK000003', '2', '2', '2', 'PE21000001', '2', '2022-01-21 08:31:41', '2022-01-21 08:30:18', '2022-01-21 08:32:04', '2022-01-21 08:44:59', 'SEWA', '6', '2022-01-26 11:35:12', 'KABEL', '0', 'IN000018', '2022-01-06 08:41:59', '2022-11-08 03:38:47'),
+('C', '1', 'X4C', 'RQ000006', 'PE000004', 'ACTIVE', '0000-00-00', 'Berhasil', '', '', '', '0', 'PK000015', '6', '6', '6', 'PE21000001', '6', '2022-01-21 05:48:45', '2022-01-07 08:02:05', '2022-01-21 05:48:49', '2022-01-21 05:58:02', 'SEWA', '6', '2022-01-21 05:58:38', 'KABEL', '0', 'IN000007', '2022-01-06 08:44:17', '2022-02-05 08:48:37');
+
+INSERT INTO `laporan_pemasangan_wifi` (`IDREPORT`, `IDPENGGUNA`, `IDPERMINTAAN`, `STATUSPASANG`, `BRG_INDOOR`, `BRG_OUTDOOR`, `SINYAL`, `JENIS`, `TESTUP`, `TESTDOWN`, `STATUS`, `BTS`, `SSID`, `FOTOTOWER`, `FOTOKABEL`, `FOTOROOTER`, `FOTOSPEED`, `FOTOFORMULIR`, `FOTOTTDFORMULIR`, `PINGGATEWAY`, `PINGGOOGLE`, `IPADDR`, `MACADDR_ANTENA`, `MACADDR_ROOTER`, `SNROOTER_FIBER`, `NOMOR_ODP`, `NOMOR_PORT_ODP`, `NOMOR_PORT_OLT`, `SIGNAL_WIRELESS`, `SIGNAL_KABEL`, `HISTORYROOTER`, `HISTORYANTENA`, `HISTORYDETAIL`, `LOKASIPEMANCAR`, `KETERANGAN`, `IDODPLOCATION`) VALUES
+('lp61e9e8fa74b92', 'PE000004', 'RQ000006', NULL, NULL, NULL, '', 'KABEL', '2.78', '2.88', NULL, NULL, NULL, NULL, NULL, '47f781eb2020ef35d13cd0dbf7203e44.jpeg', 'f730798a500aa8ec2fe6217083656835.png', 'ec93829ced40df93224314eeb63f65bb.jpeg', 'd873734bd7c68fe76967f7e6a8b111dc.jpeg', '10', '32', '10.0.77.90', '', '', 'ZTEGC7DD8857', 'Odp 8 jtwn', '1', '4', '-10', '', NULL, NULL, NULL, '', 'Tidak ada', ''),
+('lp61ea101b7d372', 'PE000003', 'RQ000005', NULL, NULL, NULL, '', 'KABEL', '4,8', '4,7', NULL, NULL, NULL, NULL, NULL, 'efa22d060d808e687cb281b599b7a96a.jpeg', '8259ec1e55133d181834c9d8810d702b.jpeg', 'e173b288468ef5bce21da6f1ad5c9a1a.jpeg', '0c347d05a0569904713f8a4df44ca556.jpeg', '1', '30', '10.3.0.70', '', '', 'ZTEGC7BE4F8E', 'ODP 6', '1', '1/2/5', '-20,5', '', NULL, NULL, NULL, '', 'installasi aman & koneksi sesuai paket yang dipesan', ''),
+('lp620271d3908cc', 'PE000002', 'RQ000004', NULL, NULL, NULL, '', 'KABEL', '8.34', '9.49', NULL, NULL, NULL, NULL, NULL, '1e0651cde159949362d4daaddd8fc774.jpeg', '326c5190f2468f26262ae6c1123b14a6.png', 'feb45b1f7321033f88d3cdf314e873c2.jpeg', '5dc72891f6f68756b5860614a80797c6.jpeg', '12', '29', '10.3.5.2', '', '', 'ZTEGCC524A29', 'Odp 13 JTWN', '2', '3', '-22.2', '', NULL, NULL, NULL, '', 'Tidak ada', '');
+
+INSERT INTO `riwayat_pelanggan` (`ID`, `IDPERMINTAAN`, `CREATEBY`, `STATUSTINDAKAN`, `STATUSLANGGANAN`, `ALASAN`, `TGLTINDAKAN`, `STATUSTINDAKANALAT`) VALUES
+('61d6a98e388', 'RQ000004', 'PE000002', 'PENGAJUAN', '', '', '2022-01-06 08:34:22', ''),
+('61d6ab57018', 'RQ000005', 'PE000003', 'PENGAJUAN', '', '', '2022-01-06 08:41:59', ''),
+('61d6abe1b3f', 'RQ000006', 'PE000004', 'PENGAJUAN', '', '', '2022-01-06 08:44:17', ''),
+('61d7910dc21', 'RQ000006', 'PE21000001', 'Dalam Proses Survei', '', '', '2022-01-07 01:02:05', ''),
+('61e9e68fd3a', 'RQ000006', '6', 'Proses Survei Selesai', '', '', '2022-01-20 22:47:43', ''),
+('61e9e6c3c7c', 'RQ000006', '6', 'Verifikasi Proses', '', '', '2022-01-20 22:48:35', ''),
+('61e9e6cd4e6', 'RQ000006', '6', 'Verifikasi Proses', '', '', '2022-01-20 22:48:45', ''),
+('61e9e6d1446', 'RQ000006', '6', 'Dalam Proses Pasang', '', '', '2022-01-20 22:48:49', ''),
+('61e9e8fa70a', 'RQ000006', '6', 'Terpasang', '', '', '2022-01-20 22:58:02', ''),
+('61e9e91ea42', 'RQ000006', '6', 'Berhasil Active', '', '', '2022-01-20 22:58:38', ''),
+('61ea0caa81d', 'RQ000005', '2', 'Dalam Proses Survei', '', '', '2022-01-21 01:30:18', ''),
+('61ea0cf408b', 'RQ000005', '2', 'Proses Survei Selesai', '', '', '2022-01-21 01:31:32', '');
+
+INSERT INTO `biaya_tagihan` (`IDBIAYA`, `IDPELANGGAN`, `IDPERMINTAAN`, `BIAYAPASANG`, `BIAYABULANAN`, `BIAYALAINLAIN`, `TOTALBIAYA`, `TGLINSERT`) VALUES
+('IN000007', 'PE000004', 'RQ000006', '0', '150000', '11000', '161000', '2022-12-01 07:53:24'),
+('IN000018', 'PE000003', 'RQ000005', '0', '165000', '11000', '176000', '2022-01-26 04:35:12'),
+('IN000035', 'PE000002', 'RQ000004', '0', '165000', '11000', '176000', '2022-11-02 04:11:20');
+
+INSERT INTO `apikeuangan_buktitransaksitagihan` (`IDUNIQ`, `IDTRANSAKSI`, `IDPERMINTAAN`, `NOINDEXTAGIHAN`, `BAYAR`, `BULANTAGIHAN`, `FLAG`, `INSERTED_AT`, `notivwa`) VALUES
+('6361f31b053', 'IN000007', 'RQ000006', '0', '150000', '2022-11-02', '2', '2022-11-02 04:33:31', '0'),
+('6361f31b0fa', 'IN000035', 'RQ000004', '0', '165000', '2022-11-02', '2', '2022-11-02 04:33:31', '0'),
+('63aa6556448', 'IN000007', 'RQ000006', '0', '150000', '2022-12-01', '0', '2022-12-27 03:24:06', '0'),
+('63aa6556491', 'IN000018', 'RQ000005', '0', '165000', '2022-01-26', '0', '2022-12-27 03:24:06', '0'),
+('63aa65564fd', 'IN000035', 'RQ000004', '0', '165000', '2022-11-02', '0', '2022-12-27 03:24:06', '0');
+
